6d5f84b7d7fd1c5b6fac9be9632cc7034ec4c4ed
jliu24 <jliu24@mit.edu>
2021-08-08 08:42:58 -0400

    about page formatting

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,7 +8,10 @@ const About = () => {
             About
           </h1>
         </div>
-      <p className="bg-white flex flex-col justify-center items-center" style={{paddingTop:"2vh", paddingBottom:"2vh", paddingLeft:"20vw", paddingRight:"20vw"}}> Rediscover is an organization which helps people rediscover the wonders of their home after the pandemic. Starting off
+        <div>
+          <img style={{ paddingTop:"1vh", paddingLeft:"29vw", paddingRight:"29vw", width:"auto"}} src="..\..\wellesley10.jpg" alt="Capezio"></img>
+        </div>
+      <p className="bg-white flex flex-col justify-center items-center" style={{paddingTop:"2vh", paddingBottom:"2vh", paddingLeft:"30vw", paddingRight:"30vw"}}> Rediscover is an organization which helps people rediscover the wonders of their home after the pandemic. Starting off
           in Wellesley, Rediscover Wellesley contains the ultimate list of stores, trails, and events in Wellesley, in addition to
           having specific information on each.</p>
     </div>
